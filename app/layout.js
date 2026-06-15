119a9b1b530e46cb24894cc20081034c94cc9a03
mcanaleees <milton.cana.me@gmail.com>
2026-06-15 10:45:06 -0500

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'VirtualHE - Tinción H&E Virtual',
+  description: 'Tinción histológica H&E virtual con IA',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  )
+}
